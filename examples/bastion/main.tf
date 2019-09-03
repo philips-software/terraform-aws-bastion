a8399bc6e4d29237297153d427a2db2de9097536
Niek Palm <dev.npalm@gmail.com>
2019-09-03 15:37:23 +0200

    Update vpc version

--- examples/bastion/main.tf
+++ examples/bastion/main.tf
@@ -7,7 +7,7 @@ provider "aws" {
 }
 
 module "vpc" {
-  source = "git::https://github.com/philips-software/terraform-aws-vpc.git?ref=terraform012"
+  source = "git::https://github.com/philips-software/terraform-aws-vpc.git?ref=2.0.0"
 
   environment = var.environment
   aws_region  = var.aws_region
